744d5d0d957f81dd656b022188e64ef92abf068d
zspears12 Spears <zaneduck1@gmail.com>
2023-10-16 18:37:53 +0000

    <platformer new>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -65,7 +65,7 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-    ceateCollectable("database", 60, 150, 0, 0)
+
 
 
 
